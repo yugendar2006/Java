ec9cefa25942b83c7c0c512f7a86e312f058b7e9
yugendar2006 <ymaladri5@gmail.com>
2026-08-13 21:37:22 +0530

    Add files via upload

---
+++ arrayCreation.java
@@ -0,0 +1,28 @@
+public class arrayCreation{
+     public static void main(String args[])
+     {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        System.out.print(arr[i]);
+     }
+}
+
+public class array1{
+    public static void main(String args[])
+    {
+    int[] arr = {1,2,3,4,5};
+    for(int i=0;i<arr.length;i++)
+        System.out.println(arr[i]);
+    }
+}
+
+public class array2{
+    public static void main(String args[])
+    {
+     for(int i=0;i<numbers.length;i++){
+        numbers[i] = sc.nextInt();
+     }
+     System.out.println(numbers[i]);
+    }
+}
+
+ 
